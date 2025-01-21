23f2c997bc9f65e6c6d04ff7f0bc885b4e59b403
gabrielfiorotti <gabrielfiorotti@users.noreply.github.com>
2025-01-21 23:37:34 +0000

    deploy: 82056cb94b4ca5210944c822dd3ff15eea4a08c1

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  getFirestore,
+  collection,
+} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARKEM5KlFNaaqyCKY97icr9wuM1q8FXBM",
+  authDomain: "webdevtrends-todo.firebaseapp.com",
+  projectId: "webdevtrends-todo",
+  storageBucket: "webdevtrends-todo.firebasestorage.app",
+  messagingSenderId: "271300124926",
+  appId: "1:271300124926:web:9f6652429f5d3284e045af",
+  measurementId: "G-VT47WFCQ1Z",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
 // Add Task
-addTaskBtn.addEventListener("click", () => {
+addTaskBtn.addEventListener("click", async () => {
   const task = taskInput.value.trim();
   if (task) {
-    const li = document.createElement("li");
-    li.textContent = task;
-    taskList.appendChild(li);
-    taskInput.value = "";
+    const taskInput = document.getElementById("taskInput");
+    const taskText = sanitizeInput(taskInput.value.trim());
+    if (taskText) {
+      await addTaskToFirestore(taskText);
+      renderTasks();
+      taskInput.value = "";
+    }
+    renderTasks();
   }
 });
+
+async function addTaskToFirestore(taskText) {
+  await addDoc(collection(db, "todos"), {
+    text: taskText,
+    completed: false,
+  });
+}
 
 // Remove Task on Click
 taskList.addEventListener("click", (e) => {
@@ -23,3 +64,52 @@ taskList.addEventListener("click", (e) => {
 window.addEventListener("error", (e) => {
   console.error("Error occurred:", e.message);
 });
+
+// Fetch tasks from Firestore when the app loads:
+
+async function renderTasks() {
+  var tasks = await getTasksFromFirestore();
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    if (!task.data().completed) {
+      const taskItem = document.createElement("li");
+      taskItem.id = task.id;
+      taskItem.textContent = task.data().text;
+      taskList.appendChild(taskItem);
+    }
+  });
+}
+async function getTasksFromFirestore() {
+  var data = await getDocs(collection(db, "todos"));
+  let userData = [];
+  data.forEach((doc) => {
+    userData.push(doc);
+  });
+  return userData;
+}
+
+// Validate user input to prevent harmful data:
+function sanitizeInput(input) {
+  const div = document.createElement("div");
+  div.textContent = input;
+  return div.innerHTML;
+}
+
+// SERVICE WORKER CONFIG
+const sw = new URL("service-worker.js", import.meta.url);
+if ("serviceWorker" in navigator) {
+  const s = navigator.serviceWorker;
+  s.register(sw.href, {
+    scope: "/INFO5146-ToDoApp/service-worker.js",
+  })
+    .then((_) =>
+      console.log(
+        "Service Worker Registered for scope:",
+        sw.href,
+        "with",
+        import.meta.url
+      )
+    )
+    .catch((err) => console.error("Service Worker Error:", err));
+}
